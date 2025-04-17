56876d63510513104f3be94ee9491692b8d25485
mgwinst <matteospatola@hotmail.com>
2025-04-17 22:42:21 +0000

    learning about low latency logging using multi-threading and lockfree queues

---
+++ tests/logging_example.cpp
@@ -0,0 +1,20 @@
+#include "include/logger.h"
+
+using namespace common;
+
+int main() {
+    
+    char c = 'd';
+    int i = 3;
+    unsigned long ul = 65;
+    float f = 3.4;
+    double d = 34.56;
+    const char* s = "test C-string";
+    std::string ss = "test string";
+
+    Logger logger("logging_example.log");
+    
+    logger.log("Logging a char:% an int:% and an unsigned int:%\n", c, i, ul);
+    
+
+}
